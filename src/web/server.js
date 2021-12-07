fd3abb452bfa3aa9b98e1c280d13d64bc31ba0fc
Daniel <danielrobinson1112@gmail.com>
2021-12-07 15:02:29 +0000

    Add server.js to send/receive from server
    Needs to be tested.

---
+++ src/web/server.js
@@ -0,0 +1,63 @@
+const IP_ADDRESS = "localhost";
+const SERVER_PORT = "3000";
+
+var socket;
+
+class Server{
+    //list of observers waiting for server updates
+    observers;
+
+    constructor(){
+        //connect and disconnect listeners
+        this.connectedToServer();
+        this.disconnectedFromServer 
+
+        //receieve listener and update observers
+        this.receiveFromServer();
+    }
+
+    //send data to server
+    sendToServer(eventName, data){
+        socket.emit(eventName, data);
+    }
+
+    //receive data from server and update oberservers
+    receiveFromServer(){
+        socket.onAny((eventName, ...args) => {
+            this.observers.forEach(element => {
+                element.update(eventName, args);
+            });
+        });
+    }
+
+    //connection setup and listener
+    connectedToServer(){
+        //connect to the socket
+        socket = io(`http://${IP_ADDRESS}:${SERVER_PORT}`);
+
+        //wait for acknowledgement
+        socket.on("connect", msg => { 
+            // do connect things //
+        });    
+    }
+
+    //disconnection listener
+    disconnectedFromServer(){
+        socket.on("disconnect", msg => {  
+            // do disconnect things //
+        });
+    }
+
+    //observer pattern, add to list
+    registerObserver(newObserver){
+        this.observers.push(newObserver);
+    }
+}
+
+//This class should be treated as abstract.
+class Observer{
+    //called whenever the server receives something
+    update(eventName, args){
+        console.log("Abstract update() called!");
+    }
+}
