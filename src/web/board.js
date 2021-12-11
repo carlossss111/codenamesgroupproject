710bba31251cd00f50a1e7cf72fb663deae78406
Daniel <danielrobinson1112@gmail.com>
2021-12-11 15:59:51 +0000

    Implemented changes from integration
    see ba70d9ad76132e60c09ced9aac8a76a74d5d44b1

--- src/web/board.js
+++ src/web/board.js
@@ -45,33 +45,40 @@ function click() {
     alert('clicked');
 }
 
-class BoardState {
 
-    cards = [[]];
+class BoardState extends Observer{
+
+    cards = [];
     clueWord;
     numOfGuesses;
     redScore;
     blueScore;
     timer = null;
     player = {
-        'team' : null,
-        'role' : null
+        "team" : null,
+        "role" : null
     };
+    turn = {
+        "team" : null,
+        "role" : null
+    }
 
     constructor()
     {
+        super();
         this.clueWord = null;
         this.numOfGuesses = null;
         this.redScore = 0;
         this.blueScore = 0; 
         let bombX = Math.floor(Math.random() * 4);
         let bombY = Math.floor(Math.random() * 4);
-        for(var i=0;i<5;i++){
-            for(var j=0;j<5;j++)
+        for(var i = 0; i < 5; i++){
+            this.cards.push([]);
+            for(var j = 0;j < 5; j++)
             {
-                this.cards.push([]);
                 
-                if(i == bombX && j == bombY){
+                if(i == bombX && j == bombY)
+                {
                     this.cards[bombX][bombY] = new Card("bombCard","bomb");
                     continue;
                 }
@@ -86,6 +93,12 @@ class BoardState {
         }
     }
 
+    //return true if it is this client's turn
+    isPlayersTurn(){
+        return (    this.player["team"] === this.turn["team"] 
+            &&      this.player["role"] === this.turn["role"]);
+    }
+
     validateClick(cardSelected,turn){
         if(cardSelected.isRevealed == true) return false;
         else if(turn.team != boardState.player.team) return false;
@@ -93,26 +106,97 @@ class BoardState {
         return true;
     }
     
-    
-    sendBoardState(Boardstate, cardSelected) {
-        server.sendToServer("SendBoardState",`Protocol : "SendBoardState"\r\nclue : "${BoardState.clueWord}"\r\n
-                            numOfGuesses : "${BoardState.numOfGuesses}"\r\nredScore : "${BoardState.redScore}"\r\n
-                            blueScore : "${BoardState.blueScore}"\r\ntimerLength : "${BoardState.timerLength}"\r\n
-                            blueScore : "${BoardState.blueScore}"\r\nplayer : "${BoardState.numOfGuesses}"\r\n
-                            player : "${BoardState.player}"\r\ncardChosen : "${cardSelected}"\r\n
-                            cards : ${BoardState.cards}"`);
+    //sends the board state to the server
+    sendBoardState(cardSelected) {
+        //find index of cardSelected
+        var i;
+        for (i = 0; i < this.cards.length; i++)
+        {
+            var j = this.cards[i].indexOf(cardSelected);
+            if(j != -1)
+                break;
+        }
+        if(j == -1)
+            throw new Error("Card selected not found!")
+        
+        //send board to server
+        server.sendToServer("sendBoardState",
+            {
+            "Protocol" : "sendBoardState",
+            "clue" : this.clueWord,
+            "numberOfGuesses" : this.numOfGuesses,
+            "redScore" : this.redScore,
+            "blueScore" : this.blueScore,
+            "timerLength" : this.timer,
+            "player" : this.player,
+            "turn" : this.turn,
+            "cardChosen" : `${i},${j}`,
+            "cards" : this.cards
+            });
         }
 
-    
-
-    sendGameOptions(hasBombCard, numOfAI, aiDifficulty, timerLength){
-        // I don't think this should be here
-    }
-
-    update(eventName, args){
-        if(eventName != "boardState")
+    //forwards to the server (and then to the other players)
+    forwardClue(){
+        //check that it is this player's turn and it is the spymaster's turn
+        if(!this.isPlayersTurn || this.turn.role != "spymaster")
             return;
         
+        /*
+        DEFINE THE CLUE HERE & VALIDATE HERE!
+        */
+
+        var clue = "placeholder";
+        var maxGuesses = 2;
+
+        //send to server
+        server.sendToServer("forwardClue",
+            {
+                "Protocol" : "forwardClue",
+                "clue" : clue,
+                "numberOfGuesses" : maxGuesses,
+                "player" : this.player,
+                "turn" : this.turn
+            })
+
+    }
+
+    //Updates when receiving server communications
+    //(Observer Class Override!)
+    update(eventName, args){
+        if(eventName == "receiveBoardState"){
+            
+            var example_cards = [];
+            for(var i = 0; i < 5; i++){
+                example_cards.push([]);
+                for(var j = 0; j < 5; j++){
+                    var randomizer1 = Math.floor(Math.random() * 3);//0-2
+                    var randomizer2 = Math.floor(Math.random() * 2);//0-1
+                    var example_colours = ["redTeam","blueTeam","neutral"];
+                    example_cards[i][j] = {"colour" : "", "word" : "placeholder", "isRevealed" : ""};
+                    example_cards[i][j].colour = example_colours[randomizer1];
+                    randomizer2 ? example_cards[i][j].isRevealed = true : example_cards[i][j].isRevealed = false;
+                }
+            }
+            const example_args = { Protocol: "receiveBoardState", clue: "placeholder", numberOfGuesses: 1, 
+                                    redScore: 1, blueScore: 1, timerLength: 100, 
+                                    turn: { team: "blue", role: "spymaster" }, cards : example_cards}
+            args = example_args;
+            console.log(args);
+
+            /*
+            UPDATE CLIENT BOARD STATE HERE!
+            */
+        }
+        else if(eventName == "forwardClue"){
+            const example_args = { Protocol: "forwardClue", clue: "placeholder", 
+                            numberOfGuesses: 2, turn: { team: "blue", role: "spymaster" } }
+            args = example_args;
+            console.log(args);
+
+            /*
+            UPDATE CLUE WORD AND DISPLAY HERE!
+            */
+        }
     }
 
     finishGame(hasWon)
@@ -129,6 +213,21 @@ class BoardState {
         }
         else return; 
     }
+
 }
 
-new BoardState();
+//randomizes the board (not cards) for debug purposes
+function DEBUG_boardRandomizer(board){
+    board.clueWord = "word";
+    board.numOfGuesses = Math.floor(Math.random() * 5); //0-4
+    board.redScore = Math.floor(Math.random() * 11); //0-10
+    board.blueScore = Math.floor(Math.random() * 11); //0-10
+    board.timer = 100;
+    Math.floor(Math.random() * 2) ? board.player.team = "red" : board.player.team = "blue" ;
+    Math.floor(Math.random() * 2) ? board.player.role = "spy" : board.player.role = "spymaster" ;
+    Math.floor(Math.random() * 2) ? board.turn.team = "red"   : board.turn.team = "blue" ;
+    Math.floor(Math.random() * 2) ? board.turn.role = "spy"   : board.turn.role = "spymaster" ;
+}
+
+var board = new BoardState();
+server.registerObserver(board);
