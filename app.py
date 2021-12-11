83ecdde32c65d7f381057a028e4f4fd96ce441c6
Daniel <danielrobinson1112@gmail.com>
2021-12-11 15:54:32 +0000

    Add placeholder protocols
    "chat", "forwardGuess" and "send/receiveBoardState"
    have been added to app.py. They now need implementation
    to change the turns and calculate stuff.

--- app.py
+++ app.py
@@ -13,7 +13,88 @@ thread_lock = Lock()
 
 @app.route('/')
 def index():
-    return render_template('html/index.html', async_mode=socket_.async_mode)
+    return render_template('html/index.html', async_mode=socket_.async_mode, cors_allowed_origins='*')
+
+"""
+Chat Protocol
+Forwards message sent from a client to all other clients.
+"""
+@socket_.on('chat', namespace='/')
+def chat_broadcast_message(messageReceived):
+    print("Chat Message Received!")
+    #define protocol and message
+    protocol = 'chat'
+    messageToSend = {
+                        'Protocol' : protocol,\
+                        'message' : messageReceived['message']\
+                    }
+    #send to client
+    emit(protocol, messageToSend, broadcast=True)
+
+"""
+forwardClue Protocol
+Forwards clue sent from a spymaster client to all other clients.
+Changes the turn.
+"""
+@socket_.on('forwardClue', namespace='/')
+def clue_broadcast_message(messageReceived):
+    print("Clue Received!")
+
+    #####################
+    #CALCULATE NEXT TURN#
+    #####################
+    nextTurn = {"team" : "?", "role" : "?"}
+    #####################
+
+    #define protocol and message
+    protocol = 'forwardClue'
+    messageToSend = {
+                        'Protocol' : protocol,\
+                        'clue' : messageReceived['clue'],\
+                        'numberOfGuesses' : messageReceived['numberOfGuesses'],\
+                        'turn' : nextTurn\
+                    }
+    #send to client
+    emit(protocol, messageToSend, broadcast=True)
+
+"""
+send/receive BoardState Protocol
+"""
+@socket_.on('sendBoardState', namespace='/')
+def chat_broadcast_message(boardReceived):
+    print("Board State Received!")
+
+    ###########################
+    #CALCULATE NEW BOARD STATE#
+    ###########################
+    numOfGuesses = 1
+    redScore = 1
+    blueScore = 1
+    nextTurn = {"team" : "?", "role" : "?"}
+
+    ###########################
+    
+    #define protocol and message
+    protocol = 'receiveBoardState'
+    messageToSend = {   
+                        'Protocol' : protocol,\
+                        'clue' : boardReceived['clue'],\
+                        'numberOfGuesses' : numOfGuesses,\
+                        'redScore' : redScore,\
+                        'blueScore' : blueScore,\
+                        'timerLength' : boardReceived['timerLength'],\
+                        'turn' : nextTurn,\
+                        'cards' : "2d array of cards"\
+                    }
+    #send to client
+    emit(protocol, messageToSend, broadcast=True)
+
+
+
+
+#test stuff
+
+
 
 
 @socket_.on('my_event', namespace='/test')
