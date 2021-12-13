e66dd6033633e4af152e21a289de7b039e30ff06
Ao LI <scyal3@exmail.nottingham.ac.uk>
2021-12-13 20:01:49 +0000

    rewrite generateBoard.py by AoLi

--- gameDev/src/generateBoard.py
+++ gameDev/src/generateBoard.py
@@ -1,46 +1,52 @@
-# Code idea coming from: https://github.com/Pbatch/Codenames
-# Refactoring and rebuild the source code to satisfy our json and html pages
-
-import numpy as np
-
+import random
 
 class generateBoard:
 
     def __init__(self, files):
+        """
+            This is the constructor.
+        """
         self.files = files
-        self.board = self.generate_board()
+        self.board = self.createWordList()
 
-    def generate_board(self):
-        # Initialize the game board
-        try:
-            all_words = [word.strip().lower() for word in open(self.files)]
-        except FileNotFoundError:
-            raise Exception("file not exist!")
-
-        permutation = np.random.permutation(len(all_words))
-        words = np.array(all_words)[permutation][:25]
+    def createWordList(self):
+        global colour
+        wordlist = []
         board = []
+        typeList = []
+        data = []
+        for a in range(9):
+            typeList.append('red')
+        for b in range(8):
+            typeList.append('blue')
+        for c in range(7):
+            typeList.append('neutral')
+        typeList.append('assassin')
+        f = open("static/data/codenames_words")
+        for line in f:
+            data.append(line.strip())
 
-        for i, word in enumerate(words):
-            if i < 9:
-                type = "blue"
-                colour = "#0080FF"
-            elif i < 17:
-                type = "red"
-                colour = "#FF0000"
-            elif i < 24:
-                type = "neutral"
-                colour = "#D0D0D0"
-            else:
-                type = "assassin"
-                colour = "#202020"
+        for a in wordlist:
+            a.replace("\n", "\r")
+        f.close()
 
-            word_details = {"name": word, "type": type, "colour": colour, "active": False}
-            board.append(word_details)
-
-        np.random.shuffle(board)
-
+        wordlist = random.sample(data, 25)
+        n = 25
         for i in range(25):
-            board[i]["id"] = i + 1
-
+            t = random.choice(range(n))
+            n = n - 1
+            type = typeList[t]
+            del typeList[t]
+            if type == 'red':
+                colour = "#0080FF"
+            elif type == 'blue':
+                colour = "#FF0000"
+            elif type == 'neutral':
+                colour = "#D0D0D0"
+            elif type == 'assassin':
+                colour = "#202020"
+            word = wordlist[i]
+            word_details = {"name": word, "type": type, "colour": colour, "active": False,"id":i+1}
+            board.append(word_details)
+        print(board)
         return board
